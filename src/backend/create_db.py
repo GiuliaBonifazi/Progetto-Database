519762dc4933ed18b426bd56322374d48f824435
GiuliaBonifazi <giulia.bonifazi@studio.unibo.it>
2024-09-11 15:10:31 +0200

    working creation query

--- src/backend/create_db.py
+++ src/backend/create_db.py
@@ -1,31 +1,8 @@
-from .database import execute
+from .database import get_database
 
 
 def create():
-    execute(
-        """
-        CREATE TABLE if not exists MEMBRO_DEL_CAST (
-            CodMembroCast INTEGER PRIMARY KEY,
-            NomeArte TEXT NOT NULL,
-            DataNascita DATE NOT NULL,
-            DataMorte DATE,
-            Attore BOOLEAN NOT NULL,
-            Regista BOOLEAN NOT NULL
-        );
-        """
-    )
-    execute(
-        """
-        CREATE TABLE if not exists FILM (
-        CodFilm INTEGER PRIMARY KEY,
-        Durata INTEGER NOT NULL,
-        Titolo TEXT NOT NULL,
-        TitoloOriginale TEXT NOT NULL,
-        Valutazione INTEGER NOT NULL,
-        AnnoUscita INTEGER NOT NULL,
-        PaeseProduzione TEXT NOT NULL,
-        CodRegista INTEGER NOT NULL,
-        FOREIGN KEY (CodRegista) REFERENCES MEMBRO_DEL_CAST (CodMembroCast)
-        );
-        """
-    )
+    with open('src/backend/db_sqlite.sql', 'r') as file:
+        script = file.read()
+    get_database().executescript(script)
+    
